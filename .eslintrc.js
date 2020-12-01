dbe3819dda2e100fa4619750e4d414fe3436b82a
Greg Rickaby <greg@gregrickaby.com>
2020-11-30 21:41:59 -0600

    ignore proptypes

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   },
   plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
   rules: {
+    'react/prop-types': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [
