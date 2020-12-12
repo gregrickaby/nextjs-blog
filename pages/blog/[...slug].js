abad2469dc0bf1e063302141616a4ba85d993cf9
Greg Rickaby <greg@gregrickaby.com>
2020-12-12 08:25:44 -0600

    use /blog/ for permalink

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,18 +1,14 @@
 import {useRouter} from 'next/router'
 import ErrorPage from 'next/error'
-import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import Layout from '../../components/layout'
-import {
-  createSinglePost,
-  getAllPosts,
-  markdownToHtml
-} from '../../lib/functions'
-import PostTitle from '../../components/post-title'
+import Container from '@/components/container'
+import PostBody from '@/components/post-body'
+import Header from '@/components/header'
+import PostHeader from '@/components/post-header'
+import Layout from '@/components/layout'
+import {createSinglePost, getAllPosts, markdownToHtml} from '@/lib/functions'
+import PostTitle from '@/components/post-title'
 import Head from 'next/head'
-import {SITE_TITLE} from '../../lib/constants'
+import {SITE_TITLE} from '@/lib/constants'
 
 export default function Post({post, preview}) {
   const router = useRouter()
