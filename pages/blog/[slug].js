759227f39a26255a7b56a328bc3341c0e3988a83
Greg Rickaby <greg@gregrickaby.com>
2020-12-12 21:14:07 -0600

    simply

---
+++ pages/blog/[slug].js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import {getPosts} from '@/lib/functions'
+
+export default function BlogPost({post}) {
+  const {
+    attributes: {coverImage, date, title},
+    html
+  } = post
+  return (
+    <article>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/atom-one-dark-reasonable.min.css"
+        />
+      </Head>
+      <Image
+        src={coverImage}
+        alt={`Cover Image for ${title}`}
+        layout="responsive"
+        width={960}
+        height={360}
+      />
+      <h1 dangerouslySetInnerHTML={{__html: title}} />
+      <time>{date}</time>
+      <div dangerouslySetInnerHTML={{__html: html}}></div>
+    </article>
+  )
+}
+
+export async function getStaticPaths() {
+  const posts = getPosts()
+
+  return {
+    paths: posts.map((post) => ({params: {slug: post.attributes.slug}})),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}) {
+  const posts = getPosts()
+
+  return {
+    props: {
+      post: posts.find((post) => post.attributes.slug === params.slug)
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,8 @@
-import Container from '@/components/container'
-import HeroPost from '@/components/hero-post'
-import Intro from '@/components/intro'
-import Layout from '@/components/layout'
-import MoreStories from '@/components/more-stories'
-import {siteTitle} from '@/lib/config'
-import {sortBlogPosts} from '@/lib/functions'
-import Head from 'next/head'
-
-export default function Index({posts}) {
-  const heroPost = posts[0]
-  const morePosts = posts.slice(1)
+export default function Home() {
   return (
     <>
-      <Layout>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+      <h1>Welcome!</h1>
+      <p>This is the main page</p>
     </>
   )
-}
-
-export async function getStaticProps() {
-  const posts = await sortBlogPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt'
-  ])
-
-  return {
-    props: {posts}
-  }
 }
