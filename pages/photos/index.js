9223ad6c59dbccb5d448ee79299071b6b104e2e2
Greg Rickaby <gregrickaby@gmail.com>
2020-12-29 13:22:16 -0600

    create photo archive page

---
+++ pages/photos/index.js
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import getPhotos from '@/functions/getPhotos'
+import Layout from '@/components/Layout'
+
+export default function PhotosPage({photos}) {
+  return (
+    <Layout>
+      <section className="grid">
+        {photos?.length &&
+          photos.map((photo, index) => {
+            return (
+              <>
+                <Image
+                  key={index}
+                  src={photo.pathRelative}
+                  height={photo.height}
+                  width={photo.width}
+                  layout="fixed"
+                />
+                <p>{photo?.description}</p>
+              </>
+            )
+          })}
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const photos = await getPhotos()
+
+  return {
+    props: {
+      photos
+    }
+  }
+}
