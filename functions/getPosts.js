6eba8a9f1a561ea44c626807d84cc76284487e6e
Greg Rickaby <gregrickaby@gmail.com>
2020-12-29 10:48:46 -0600

    rename functions.js to getPosts.js

---
+++ functions/getPosts.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const marked = require('marked')
+const fm = require('front-matter')
+const hljs = require('highlight.js')
+import {postDirectory} from '@/functions/config'
+
+export default function getPosts(allPosts = []) {
+  marked.setOptions({
+    highlight: (code, lang) => {
+      if (lang) {
+        return hljs.highlight(lang, code).value
+      }
+
+      return code
+    }
+  })
+
+  const postFiles = fs.readdirSync(postDirectory)
+
+  for (let i = 0; i < postFiles.length; i++) {
+    const postContent = fs.readFileSync(`${postDirectory}/${postFiles[i]}`, {
+      encoding: 'utf-8'
+    })
+
+    const {body, ...frontMatter} = fm(postContent)
+
+    allPosts.push({
+      html: marked(body),
+      ...frontMatter
+    })
+  }
+
+  const posts = allPosts.sort((post1, post2) => {
+    return post1.attributes.date > post2.attributes.date ? '-1' : '1'
+  })
+
+  return posts
+}
