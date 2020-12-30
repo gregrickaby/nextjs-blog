b639a819e3572c8b266a390efaef075434a8c487
Greg Rickaby <gregrickaby@gmail.com>
2020-12-29 19:49:50 -0600

    add link to photos

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,18 @@ export default function Home() {
   return (
     <Layout>
       <h1>Welcome!</h1>
-      <p>
-        Visit the{' '}
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-      </p>
+      <ul>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/photos">
+            <a>Photos</a>
+          </Link>
+        </li>
+      </ul>
     </Layout>
   )
 }
