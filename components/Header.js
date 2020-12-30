53c06f17c3c748055c4f7eb270c8a2ebe90f10f5
Greg Rickaby <gregrickaby@gmail.com>
2020-12-29 20:33:38 -0600

    import <Nav> and style header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,21 @@
+import Nav from '@/components/Nav'
 import config from '@/functions/config'
 import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="space-y-2">
-      <h1 className="text-4xl">
-        <Link href="/">
-          <a>{config.siteName}</a>
-        </Link>
-      </h1>
-      <p className="text-1xl">{config.siteDescription}</p>
+    <header className="flex justify-between space-y-2">
+      <div className="flex flex-col space-y-2">
+        <h1 className="text-4xl">
+          <Link href="/">
+            <a>{config.siteName}</a>
+          </Link>
+        </h1>
+        <p className="text-1xl">{config.siteDescription}</p>
+      </div>
+      <div>
+        <Nav />
+      </div>
     </header>
   )
 }
