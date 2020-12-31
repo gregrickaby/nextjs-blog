edc0356a98afd234d595491f82e5d1da6c315d4a
Greg Rickaby <gregrickaby@gmail.com>
2020-12-31 16:46:38 -0600

    delete and use CSS instead

--- components/Article.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export default function Article({children}) {
-  return <article className="space-y-4">{children}</article>
-}
-
-Article.propTypes = {
-  children: PropTypes.any.isRequired
-}
