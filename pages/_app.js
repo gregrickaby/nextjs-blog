c15abad47de13bfdb8a4929628df65e8e99956dc
Greg Rickaby <gregrickaby@gmail.com>
2021-01-01 15:10:37 -0600

    bring in next-seo. set up <DefaultSeo />

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
-import '@/styles/tailwind.css'
 import '@/styles/prism.css'
-
+import '@/styles/tailwind.css'
+import config from '@/functions/config'
+import {DefaultSeo} from 'next-seo'
 import PropTypes from 'prop-types'
 
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: config?.siteUrl,
+          site_name: config?.siteName
+        }}
+        twitter={{
+          handle: `${config?.socialNetworks[1]?.handle}`,
+          site: `${config?.socialNetworks[1]?.handle}`,
+          cardType: 'summary_large_image'
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 App.propTypes = {
