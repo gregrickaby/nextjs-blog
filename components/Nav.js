27be9e52f561f211822ff69426665cab713ab67a
Greg Rickaby <gregrickaby@gmail.com>
2020-12-31 20:58:01 -0600

    check nested key

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import config from '@/functions/config'
 export default function Nav() {
   return (
     <nav className="align-center space-x-4">
-      {config?.length &&
+      {config.navigation?.length &&
         config?.navigation.map((nav, index) => {
           return (
             <Link key={index} href={nav?.url}>
