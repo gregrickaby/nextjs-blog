db5797fa923b039bafbc38ff964f24aa8c8cab0b
Greg Rickaby <gregrickaby@gmail.com>
2020-12-31 20:43:15 -0600

    add avatar. simplify markup

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,26 @@
 import Nav from '@/components/Nav'
 import config from '@/functions/config'
-import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className="flex flex-col text-center md:flex-row justify-between space-y-2">
-      <div className="flex flex-col">
-        <h1 className="text-4xl font-sans">
-          <Link href="/">
-            <a className="no-underline">{config.siteName}</a>
-          </Link>
-        </h1>
-        <p className="text-1xl">{config.siteDescription}</p>
+    <header className="flex flex-col md:flex-row justify-between text-center">
+      <div className="md:flex">
+        <Image
+          alt={config.siteAuthor}
+          className="rounded-full"
+          height="96"
+          layout="fixed"
+          quality="100"
+          src={config.authorAvatar}
+          width="96"
+        />
+        <div className="flex flex-col justify-center md:ml-4 font-sans mb-4 md:mx-0">
+          <h1 className="text-4xl leading-none">{config?.siteName}</h1>
+          <p>{config?.siteDescription}</p>
+        </div>
       </div>
-      <div>
-        <Nav />
-      </div>
+      <Nav />
     </header>
   )
 }
