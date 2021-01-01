b6fb33cb1e145989962f41aded07a206a1daa006
Greg Rickaby <gregrickaby@gmail.com>
2021-01-01 13:38:20 -0600

    use getPostData

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,32 @@
+import Article from '@/components/Article'
 import Layout from '@/components/Layout'
 import {PAGES_PATH} from '@/functions/getMdx'
-import a11yEmoji from '@fec/remark-a11y-emoji'
-import fs from 'fs'
-import matter from 'gray-matter'
+import {getPostData} from '@/functions/getPosts'
 import hydrate from 'next-mdx-remote/hydrate'
-import renderToString from 'next-mdx-remote/render-to-string'
-import path from 'path'
-import oembed from 'remark-oembed'
-import prism from 'remark-prism'
+
 /**
  * Dynamically import components into MDX files.
+ *
+ * @see https://github.com/vercel/next.js/tree/canary/examples/with-mdx-remote#conditional-custom-components
  */
 const components = {}
 
-export default function Home({source}) {
+export default function Homepage({source, frontMatter}) {
   const content = hydrate(source, {components})
   return (
     <Layout>
-      <main className="space-y-4">{content}</main>
+      <Article frontMatter={frontMatter}>{content}</Article>
     </Layout>
   )
 }
 
-export const getStaticProps = async () => {
-  const postFilePath = path.join(PAGES_PATH, 'homepage.mdx')
-  const source = fs.readFileSync(postFilePath)
-  const {content, data} = matter(source)
-  const mdxSource = await renderToString(content, {
-    components,
-    mdxOptions: {
-      remarkPlugins: [
-        a11yEmoji,
-        [oembed, {syncWidget: true}],
-        [
-          prism,
-          {
-            transformInlineCode: true,
-            plugins: [
-              'autolinker',
-              'command-line',
-              'clipboard',
-              'data-uri-highlight',
-              'diff-highlight',
-              'inline-color',
-              'keep-markup',
-              'line-numbers',
-              'show-invisibles',
-              'treeview'
-            ]
-          }
-        ]
-      ]
-    },
-    scope: data
-  })
+export async function getStaticProps() {
+  const post = await getPostData(PAGES_PATH, 'homepage', components)
 
   return {
     props: {
-      source: mdxSource,
-      frontMatter: data
+      source: post?.mdx,
+      frontMatter: post?.data
     }
   }
 }
