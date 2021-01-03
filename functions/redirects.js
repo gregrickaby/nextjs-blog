f9f504e1e560641a1718ad7c80ee66fc04a4f1b3
Greg Rickaby <gregrickaby@gmail.com>
2021-01-03 13:41:37 -0600

    build list of redirects

---
+++ functions/redirects.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    source: '/2020/06/reddit-image-viewer/',
+    destination: '/blog/reddit-image-viewer',
+    permanent: true
+  },
+  {
+    source: '/2013/05/remove-woocommerce-styles-and-scripts/',
+    destination: '/blog/remove-woocommerce-styles-and-scripts',
+    permanent: true
+  },
+  {
+    source: '/2013/05/safely-update-an-ubuntu-ec2-instance-on-amazon-aws/',
+    destination: '/',
+    permanent: true
+  },
+  {
+    source: '/2016/06/modify-wordpress-custom-post-type/',
+    destination: '/blog/modify-wordpress-custom-post-type',
+    permanent: true
+  },
+  {
+    source: '/2013/05/the-perfect-apc-configuration/',
+    destination: '/blog/the-perfect-apc-configuration',
+    permanent: true
+  },
+  {
+    source: '/2013/05/using-the-golden-ratio-and-rems/',
+    destination: '/blog/using-the-golden-ratio-and-rems',
+    permanent: true
+  },
+  {
+    source: '/2013/05/genesis-code-snippets/',
+    destination: '/blog/genesis-code-snippets',
+    permanent: true
+  },
+  {
+    source: '/2013/05/microphone-shootout-behringer-b-1-vs-neumann-tlm-103/',
+    destination: '/blog/microphone-shootout-behringer-b-1-vs-neumann-tlm-103',
+    permanent: true
+  },
+  {
+    source: '/2020/06/displaying-exif-data-in-wordpress/',
+    destination: '/blog/displaying-exif-data-in-wordpress',
+    permanent: true
+  },
+  {
+    source: '/2017/06/podcasting-sound-like-a-radio-dj/',
+    destination: '/blog/podcasting-sound-like-a-radio-dj',
+    permanent: true
+  },
+  {
+    source: '/2013/10/create-mu-plugin-for-wordpress/',
+    destination: '/blog/create-mu-plugin-for-wordpress',
+    permanent: true
+  },
+  {
+    source: '/2014/11/set-up-localhost-on-virtualbox/',
+    destination: '/blog/set-up-localhost-on-virtualbox',
+    permanent: true
+  },
+  {
+    source: '/2020/06/swr-examples/',
+    destination: '/blog/swr-examples',
+    permanent: true
+  },
+  {
+    source: '/about/',
+    destination: '/',
+    permanent: true
+  },
+  {
+    source: '/category/:slug*',
+    destination: '/',
+    permanent: true
+  },
+  {
+    source: '/tag/:slug*',
+    destination: '/',
+    permanent: true
+  }
+]
--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
+const redirects = require('./functions/redirects')
+
 module.exports = {
   webpack(config) {
     config.node = {fs: 'empty'} // Fixes NPM packages that depend on the `fs` module.
     return config
   },
   async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true
-      }
-    ]
+    return redirects
   }
 }
