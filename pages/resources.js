31f1b4764443b7e5e2486966ad036fa39a13cb18
Greg Rickaby <gregrickaby@gmail.com>
2021-01-03 13:42:05 -0600

    add contact and resources pages

---
+++ pages/resources.js
@@ -0,0 +1,36 @@
+import Layout from '@/components/Layout'
+import ArchiveHeader from '@/components/ArchiveHeader'
+import config from '@/functions/config'
+import {PAGES_PATH} from '@/functions/getMdx'
+import {getPostData} from '@/functions/getPosts'
+import hydrate from 'next-mdx-remote/hydrate'
+import {NextSeo} from 'next-seo'
+
+/**
+ * Dynamically import components into MDX files.
+ *
+ * @see https://github.com/vercel/next.js/tree/canary/examples/with-mdx-remote#conditional-custom-components
+ */
+const components = {}
+
+export default function ResourcesPage({source, frontMatter}) {
+  const content = hydrate(source, {components})
+  return (
+    <Layout>
+      <NextSeo title={config?.siteName} description={frontMatter?.excerpt} />
+      <ArchiveHeader title={frontMatter?.title} />
+      <article>{content}</article>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const post = await getPostData(PAGES_PATH, 'resources', components)
+
+  return {
+    props: {
+      source: post?.mdx,
+      frontMatter: post?.data
+    }
+  }
+}
