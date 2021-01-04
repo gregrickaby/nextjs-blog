37e1636d62283cee9db1675e17a24857101ecded
Greg Rickaby <gregrickaby@gmail.com>
2021-01-03 21:38:38 -0600

    display post date

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
 
-export default function PostHeader(props) {
+export default function PostHeader({props}) {
   return (
     <header className="text-center pb-4">
-      <h1 className="mb-3" dangerouslySetInnerHTML={{__html: props?.title}} />
-      {props?.excerpt && (
+      <time
+        className="font-sans text-sm text-gray-500"
+        dateTime={dayjs(props?.frontMatter?.date).toISOString()}
+      >
+        {dayjs(props?.frontMatter?.date).format('MMM DD, YYYY')}
+      </time>
+      <h1
+        className="mb-3"
+        dangerouslySetInnerHTML={{__html: props?.frontMatter?.title}}
+      />
+      {props?.frontMatter?.excerpt && (
         <p
-          className="text-gray-500"
-          dangerouslySetInnerHTML={{__html: props?.excerpt}}
+          className="font-sans leading-tight tracking-tight text-gray-500"
+          dangerouslySetInnerHTML={{__html: props?.frontMatter?.excerpt}}
         />
       )}
       <hr />
@@ -16,6 +26,7 @@ export default function PostHeader(props) {
 }
 
 PostHeader.propTypes = {
+  date: PropTypes.string,
   excerpt: PropTypes.string,
   title: PropTypes.string.isRequired
 }
