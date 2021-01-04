c238b18d2c3afa8668250255cb69aa921b38c0a0
Greg Rickaby <gregrickaby@gmail.com>
2021-01-03 21:38:29 -0600

    pass all props

--- components/Article.js
+++ components/Article.js
@@ -2,13 +2,11 @@ import PostHeader from '@/components/PostHeader'
 import PropTypes from 'prop-types'
 
 export default function Article(props) {
+  console.log(props)
   return (
     <>
       <article>
-        <PostHeader
-          title={props?.frontMatter?.title}
-          excerpt={props?.frontMatter?.excerpt}
-        />
+        <PostHeader props={props} />
         {props.children}
       </article>
     </>
