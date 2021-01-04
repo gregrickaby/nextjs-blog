3b50ec17c9e1d86d7985a9fdfc13aaaabdecd694
Greg Rickaby <gregrickaby@gmail.com>
2021-01-03 21:41:10 -0600

    remove console.log

--- components/Article.js
+++ components/Article.js
@@ -2,14 +2,11 @@ import PostHeader from '@/components/PostHeader'
 import PropTypes from 'prop-types'
 
 export default function Article(props) {
-  console.log(props)
   return (
-    <>
-      <article>
-        <PostHeader props={props} />
-        {props.children}
-      </article>
-    </>
+    <article>
+      <PostHeader props={props} />
+      {props.children}
+    </article>
   )
 }
 
