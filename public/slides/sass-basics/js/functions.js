43ce976af691dbacfd92b60f20673980158de2b0
Greg Rickaby <gregrickaby@gmail.com>
2021-01-05 09:52:56 -0600

    check in /slides

---
+++ public/slides/sass-basics/js/functions.js
@@ -0,0 +1,27 @@
+	/**
+	 * google fonts
+	 */
+	( function($) {
+		WebFontConfig = { google: {
+			families: [ 'Open+Sans:300italic,400italic,700italic,300,400,700:latin' ]
+		} };
+
+	( function() {
+
+		var wf = document.createElement( 'script' );
+		wf.src = ( 'https:' == document.location.protocol ? 'https' : 'http' ) +
+	     '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		wf.type  = 'text/javascript';
+		wf.async = 'true';
+
+		var s = document.getElementsByTagName( 'script' )[0];
+		s.parentNode.insertBefore( wf, s );
+
+	})();
+
+	/**
+	 * slideshow engine
+	 */
+	var slideshow = new SlideShow();
+
+})(jQuery);
