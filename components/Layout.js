e4e673f17248e567791cb0fe2fa8a8a0deb5f113
Greg Rickaby <gregrickaby@gmail.com>
2021-01-05 15:54:08 -0600

    move <NextSeo /> here. setup proptypes

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,16 @@ import PropTypes from 'prop-types'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Meta from '@/components/Meta'
+import {NextSeo} from 'next-seo'
 
-export default function Layout({children}) {
+export default function Layout({children, ...props}) {
   return (
     <>
+      <NextSeo
+        title={props?.title}
+        description={props?.description}
+        openGraph={props?.openGraph}
+      />
       <Meta />
       <div className="container space-y-12 max-w-2xl mx-auto p-8 md:px-0 py-8">
         <Header />
@@ -17,5 +23,8 @@ export default function Layout({children}) {
 }
 
 Layout.propTypes = {
-  children: PropTypes.any.isRequired
+  children: PropTypes.any.isRequired,
+  description: PropTypes.string.isRequired,
+  openGraph: PropTypes.object,
+  title: PropTypes.string.isRequired
 }
