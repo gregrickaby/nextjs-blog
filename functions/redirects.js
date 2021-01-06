61c94092b015dec1eb7f2c626a985d072e64802f
Greg Rickaby <gregrickaby@gmail.com>
2021-01-06 16:43:38 -0600

    create redirect to rss

--- functions/redirects.js
+++ functions/redirects.js
@@ -73,5 +73,10 @@ module.exports = [
     source: '/tag/:slug*',
     destination: '/',
     permanent: true
+  },
+  {
+    source: '/feed',
+    destination: '/rss.xml',
+    permanent: true
   }
 ]
