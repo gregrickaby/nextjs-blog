e33ca4e341c733410c8d19830ee70aac42bdf2fc
Greg Rickaby <gregrickaby@gmail.com>
2021-01-08 12:50:44 -0600

    create <DisplayOptions /> component

---
+++ components/DisplayOptions.js
@@ -0,0 +1,77 @@
+import cn from 'classnames'
+import {useEffect, useState} from 'react'
+
+export default function DisplayOptions() {
+  const [fontFamily, setFontFamily] = useState('font-sans')
+  const [fontSelector, toggleFontSelector] = useState(false)
+
+  function clearFonts() {
+    document.body.classList.remove('font-sans')
+    document.body.classList.remove('font-mono')
+    document.body.classList.remove('font-serif')
+    document.body.classList.remove('font-comic')
+    document.body.classList.remove('font-dyslexic')
+  }
+
+  function toggleDisplayOptions(event) {
+    event.preventDefault()
+    toggleFontSelector((prev) => !prev)
+  }
+
+  function changeFont(event) {
+    event.preventDefault()
+    clearFonts()
+    setFontFamily(event.target.value)
+    document.body.classList.add(event.target.value)
+    localStorage.setItem('font', event.target.value)
+    toggleFontSelector(false)
+  }
+
+  function setFontOnLoad() {
+    const font = localStorage.getItem('font')
+    const validateFont = !!font && font.length > 0 ? font : ''
+    if (validateFont) {
+      setFontFamily(validateFont)
+      clearFonts()
+      document.body.classList.add(validateFont)
+    }
+  }
+
+  useEffect(() => {
+    setFontOnLoad()
+  }, [])
+
+  return (
+    <div
+      className={cn('flex items-center fixed top-0 right-0 p-2', {
+        'dark:bg-gray-900 bg-white': fontSelector
+      })}
+    >
+      <form onSubmit={toggleDisplayOptions}>
+        <label htmlFor="display" className="sr-only">
+          toggle display options
+        </label>
+        <button id="display" className="bg-transparent px-2">
+          {fontSelector ? 'X' : 'Aa'}
+        </button>
+      </form>
+
+      {fontSelector && (
+        <div className="flex ml-2">
+          <select
+            id="fontSelect"
+            className="p-2 dark:text-gray-900"
+            value={fontFamily}
+            onChange={changeFont}
+          >
+            <option value="font-sans">sans-serif</option>
+            <option value="font-serif">serif</option>
+            <option value="font-mono">monospace</option>
+            <option value="font-comic">comic sans</option>
+            <option value="font-dyslexic">open dyslexic</option>
+          </select>
+        </div>
+      )}
+    </div>
+  )
+}
