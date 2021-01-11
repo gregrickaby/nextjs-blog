3453b939ffd732f502cae9b532b8c998ae3b820a
Greg Rickaby <gregrickaby@gmail.com>
2021-01-11 09:16:56 -0600

    user-scalable=no

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,10 @@ import Head from 'next/head'
 export default function Meta() {
   return (
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0 user-scalable=no"
+      />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <link
         rel="alternate"
