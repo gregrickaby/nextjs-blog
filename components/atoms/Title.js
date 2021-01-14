d7a8aa631026b34522525483a0a4bab38424f887
Greg Rickaby <gregrickaby@gmail.com>
2021-01-14 16:01:44 -0600

    update props

--- components/atoms/Title.js
+++ components/atoms/Title.js
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types'
  * Render the Title component.
  *
  * @author Greg Rickaby
- * @param {object} props      The component attributes as props.
- * @param {string} props.text Any text to display in the title.
+ * @param {object} props       The component attributes as props.
+ * @param {string} props.title Any text to display in the title.
  * @return {Element}           The Title component.
  */
 export default function Title(props) {
   return (
     <h1
       className="title wide text-gradient"
-      dangerouslySetInnerHTML={{__html: props?.text}}
+      dangerouslySetInnerHTML={{__html: props?.title}}
     />
   )
 }
 
 Title.propTypes = {
-  text: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired
 }
