2c497ee1baeeb85011f85bf3884dae36632d2972
Greg Rickaby <gregrickaby@gmail.com>
2021-01-14 08:31:17 -0600

    configure storybook

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+/**
+ * Configure Storybook.
+ *
+ * @see https://storybook.js.org/docs/react/configure/overview
+ */
+module.exports = {
+  reactOptions: {
+    fastRefresh: true,
+    strictMode: true
+  },
+  stories: ['../components/**/**/*.stories.@(js|mdx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    'storybook-css-modules-preset'
+  ],
+  webpackFinal: async (config) => {
+    /**
+     * Enable @ symbol aliases located in jsconfig.json
+     */
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../')
+    }
+
+    return config
+  }
+}
