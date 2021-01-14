8cba52061281a61fe1f6963342130f4b49a6d190
Greg Rickaby <gregrickaby@gmail.com>
2021-01-13 21:11:45 -0600

    add [slug].js

---
+++ pages/[slug].js
@@ -0,0 +1,87 @@
+import PropTypes from 'prop-types'
+import Layout from '@/components/Layout'
+import ArchiveHeader from '@/components/ArchiveHeader'
+import config from '@/functions/config'
+import {PAGES_PATH} from '@/functions/getMdx'
+import {getPostData, getPostsPath} from '@/functions/getPosts'
+import hydrate from 'next-mdx-remote/hydrate'
+import Image from 'next/image'
+
+/**
+ * Pass components into MDX files.
+ *
+ * @see https://github.com/vercel/next.js/tree/canary/examples/with-mdx-remote#conditional-custom-components
+ */
+const components = {Image}
+
+/**
+ * Render the BlogPost component.
+ *
+ * @author Greg Rickaby
+ * @param {object} props             The component attributes as props.
+ * @param {object} props.source      The post content.
+ * @param {object} props.frontMatter The post meta data.
+ * @return {Element}                 The BlogPost component.
+ */
+export default function BlogPost({source, frontMatter}) {
+  const content = hydrate(source, {components})
+  return (
+    <Layout
+      title={`${frontMatter.title} - ${config?.siteName}`}
+      description={frontMatter?.excerpt}
+      openGraph={{
+        title: `${frontMatter.title} - ${config?.siteName}`,
+        description: frontMatter?.excerpt,
+        images: [
+          {
+            url: `${config.siteUrl}${frontMatter?.coverImage}`,
+            alt: frontMatter?.title
+          }
+        ]
+      }}
+    >
+      <ArchiveHeader title={frontMatter?.title} />
+      <article>{content}</article>
+    </Layout>
+  )
+}
+
+BlogPost.propTypes = {
+  frontMatter: PropTypes.object.isRequired,
+  source: PropTypes.object.isRequired
+}
+
+/**
+ * Get static paths.
+ *
+ * @author Greg Rickaby
+ * @return {object} All page paths.
+ */
+export async function getStaticPaths() {
+  const paths = getPostsPath(PAGES_PATH)
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+/**
+ * Get static props.
+ *
+ * @author Greg Rickaby
+ * @see https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation
+ * @param {object} context        Incoming context.
+ * @param {object} context.params The route parameters.
+ * @return {object}               All page props.
+ */
+export async function getStaticProps({params}) {
+  const post = await getPostData(PAGES_PATH, params.slug, components)
+
+  return {
+    props: {
+      source: post?.mdx,
+      frontMatter: post?.data
+    }
+  }
+}
