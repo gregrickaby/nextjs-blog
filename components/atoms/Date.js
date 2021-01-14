2f5bf66f3eec58d301318433fe958517a0bcc5c5
Greg Rickaby <gregrickaby@gmail.com>
2021-01-14 15:56:06 -0600

    create <Date /> component

---
+++ components/atoms/Date.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
+
+/**
+ * Render the Date component.
+ *
+ * @author Greg Rickaby
+ * @param {object} props      The component attributes as props.
+ * @param {string} props.date The post date.
+ * @return {Element}          The Date component.
+ */
+export default function Date(props) {
+  return (
+    <time
+      className="font-sans text-gray-500 dark:text-gray-100"
+      dateTime={dayjs(props?.date).toISOString()}
+    >
+      {dayjs(props?.date).format('MMM DD, YYYY')}
+    </time>
+  )
+}
+
+Date.propTypes = {
+  date: PropTypes.string.isRequired
+}
