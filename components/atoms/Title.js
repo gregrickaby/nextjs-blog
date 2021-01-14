9b43c7639c9a0fca3afba0b0921d452f2e5ad744
Greg Rickaby <gregrickaby@gmail.com>
2021-01-14 14:43:07 -0600

    create <Title /> component

---
+++ components/atoms/Title.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+/**
+ * Render the Title component.
+ *
+ * @author Greg Rickaby
+ * @param {object} props      The component attributes as props.
+ * @param {string} props.text Any text to display in the title.
+ * @return {Element}           The Title component.
+ */
+export default function Title(props) {
+  return (
+    <h1
+      className="title wide"
+      dangerouslySetInnerHTML={{__html: props?.text}}
+    />
+  )
+}
+
+Title.propTypes = {
+  text: PropTypes.string.isRequired
+}
