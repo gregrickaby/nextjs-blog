022f225874c37269547396ed8a066b99726cefbf
Greg Rickaby <gregrickaby@gmail.com>
2021-01-25 20:26:11 -0600

    update card layout

--- components/molecules/Card/Card.js
+++ components/molecules/Card/Card.js
@@ -1,7 +1,8 @@
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import styles from './Card.module.css'
 import cn from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
+import PropTypes from 'prop-types'
+import styles from './Card.module.css'
 
 /**
  * Render the Card component.
@@ -11,42 +12,58 @@ import cn from 'classnames'
  * @param {object} props.data     The post data.
  * @param {string} props.filePath The MDX file path
  * @param {string} props.path     The post baseline route, e.g. /blog
- * @return {Element}              The Card component.
+ * @return {Element} The Card component.
  */
 export default function Card(props) {
-  const backgroundGradient = {
-    backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1)), url(${props?.data?.coverImage})`
-  }
   return (
-    <div className={styles.card}>
-      <div
-        className={cn(styles.backgroundWrap, 'bg-size')}
-        style={backgroundGradient}
-      >
-        <div className={styles.cardInner}>
-          <Link
-            as={`/${props?.path}/${props?.data?.slug}`}
-            href={`/${props?.path}/[slug]`}
-          >
-            <a className={styles.cardLink}>
-              <h3
-                className={styles.cardTitle}
-                dangerouslySetInnerHTML={{__html: props?.data?.title}}
-              />
-            </a>
-          </Link>
-        </div>
+    <article className={styles.card}>
+      <div className={styles.backgroundWrap}>
+        <Link
+          as={`/${props?.path}/${props?.data?.slug}`}
+          href={`/${props?.path}/[slug]`}
+        >
+          <a className={styles.cardLink}>
+            <Image
+              alt={props?.data?.title}
+              className={styles.image}
+              fill="fill"
+              height="179"
+              src={props?.data?.coverImage}
+              width="224"
+            />
+          </a>
+        </Link>
       </div>
-      <p
-        className={cn(styles.cardExcerpt, 'dark:text-gray-100')}
-        dangerouslySetInnerHTML={{__html: props?.data?.excerpt}}
-      />
-    </div>
+
+      <div className={styles.cardInner}>
+        {!!props?.data?.category && (
+          <p className={cn(styles.cardMeta, 'text-sm')}>
+            {props?.data?.category}
+          </p>
+        )}
+        <Link
+          as={`/${props?.path}/${props?.data?.slug}`}
+          href={`/${props?.path}/[slug]`}
+        >
+          <a className={styles.cardLink}>
+            <h3
+              className={styles.cardTitle}
+              dangerouslySetInnerHTML={{__html: props?.data?.title}}
+            />
+          </a>
+        </Link>
+        <p
+          className={cn(styles.cardExcerpt, 'dark:text-gray-100')}
+          dangerouslySetInnerHTML={{__html: props?.data?.excerpt}}
+        />
+      </div>
+    </article>
   )
 }
 
 Card.propTypes = {
   data: PropTypes.shape({
+    category: PropTypes.string,
     coverImage: PropTypes.string,
     excerpt: PropTypes.string,
     slug: PropTypes.string,
