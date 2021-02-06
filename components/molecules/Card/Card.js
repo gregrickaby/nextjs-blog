d8bb33b0b27d6c73409eae1dbe3fe147db449e4f
Greg Rickaby <gregrickaby@gmail.com>
2021-02-06 11:00:34 -0600

    fix prop type

--- components/molecules/Card/Card.js
+++ components/molecules/Card/Card.js
@@ -58,7 +58,7 @@ export default function Card(props) {
 
 Card.propTypes = {
   data: PropTypes.shape({
-    category: PropTypes.string,
+    category: PropTypes.array,
     coverImage: PropTypes.string,
     excerpt: PropTypes.string,
     slug: PropTypes.string,
