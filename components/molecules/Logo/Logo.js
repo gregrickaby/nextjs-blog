6ff65317de739f95ed242705e5514dab246dcb1c
Greg Rickaby <gregrickaby@gmail.com>
2021-02-24 07:19:33 -0600

    move logo to <Logo />

---
+++ components/molecules/Logo/Logo.js
@@ -0,0 +1,29 @@
+import config from '@/functions/config'
+import cn from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './Logo.module.css'
+
+export default function Logo() {
+  return (
+    <div className={styles.logoWrap}>
+      <Link href="/">
+        <a className={styles.logoLink}>
+          <Image
+            alt={config?.siteAuthor}
+            className={styles.logo}
+            height="80"
+            src={config?.authorAvatar}
+            width="80"
+          />
+        </a>
+      </Link>
+      <div className={styles.titleWrap}>
+        <h1 className={styles.title}>{config?.siteName}</h1>
+        <p className={cn(styles.description, 'dark:text-gray-400')}>
+          {config?.siteDescription}
+        </p>
+      </div>
+    </div>
+  )
+}
--- components/organisms/Header/Header.js
+++ components/organisms/Header/Header.js
@@ -1,8 +1,5 @@
 import HeaderNavigation from '@/components/molecules/HeaderNavigation/HeaderNavigation'
-import config from '@/functions/config'
-import cn from 'classnames'
-import Image from 'next/image'
-import Link from 'next/link'
+import Logo from '@/components/molecules/Logo/Logo'
 import styles from './Header.module.css'
 
 /**
@@ -14,25 +11,7 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logoWrap}>
-        <Link href="/">
-          <a className={styles.logoLink}>
-            <Image
-              alt={config?.siteAuthor}
-              className={styles.logo}
-              height="80"
-              src={config?.authorAvatar}
-              width="80"
-            />
-          </a>
-        </Link>
-        <div className={styles.titleWrap}>
-          <h1 className={styles.title}>{config?.siteName}</h1>
-          <p className={cn(styles.description, 'dark:text-gray-400')}>
-            {config?.siteDescription}
-          </p>
-        </div>
-      </div>
+      <Logo />
       <HeaderNavigation />
     </header>
   )
