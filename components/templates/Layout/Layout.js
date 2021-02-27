c19ebcf5896daacc5cc02d9a65af7285f09afdcc
Greg Rickaby <gregrickaby@gmail.com>
2021-02-27 09:24:23 -0600

    create wide layout option

--- components/templates/Layout/Layout.js
+++ components/templates/Layout/Layout.js
@@ -1,6 +1,7 @@
 import Meta from '@/components/common/Meta'
 import Footer from '@/components/organisms/Footer/Footer'
 import Header from '@/components/organisms/Header/Header'
+import cn from 'classnames'
 import {NextSeo} from 'next-seo'
 import PropTypes from 'prop-types'
 import styles from './Layout.module.css'
@@ -15,6 +16,7 @@ import styles from './Layout.module.css'
  * @return {Element}              The Layout component.
  */
 export default function Layout({children, ...props}) {
+  const layout = props?.layout == 'wide' ? styles.layoutWide : styles.layout
   return (
     <>
       <NextSeo
@@ -24,7 +26,7 @@ export default function Layout({children, ...props}) {
       />
       <Meta />
       <Header />
-      <div className={styles.layout}>
+      <div className={cn(layout)}>
         <main>{children}</main>
         <Footer />
       </div>
@@ -35,6 +37,7 @@ export default function Layout({children, ...props}) {
 Layout.propTypes = {
   children: PropTypes.any.isRequired,
   description: PropTypes.string.isRequired,
+  layout: PropTypes.string,
   openGraph: PropTypes.object,
   title: PropTypes.string.isRequired
 }
