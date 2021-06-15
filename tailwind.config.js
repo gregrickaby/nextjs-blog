2b94954bffbd7603909dc7742056e2ff4022ba45
Greg Rickaby <gregrickaby@gmail.com>
2021-06-15 09:40:37 -0500

    adjust fonts for Suez One

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        comicneue: ['Comic Neue', 'sans-serif'],
-        dancingscript: ['Dancing Script', 'cursive'],
-        mono: ['Roboto Mono', 'monospace'],
-        opendyslexic: ['Open Dyslexic', 'sans-serif'],
         sans: ['Roboto', 'sans-serif'],
-        serif: ['Roboto Slab', 'serif']
+        serif: ['Suez One', 'serif']
       },
       colors: {
         gray: {
@@ -43,36 +39,50 @@ module.exports = {
     plugin(function ({addBase, theme}) {
       addBase({
         h1: {
-          fontSize: theme('fontSize.4xl'),
-          marginBottom: '2.1rem',
-          lineHeight: '1.78'
+          fontFamily: theme('fontFamily.serif'),
+          fontSize: theme('fontSize.5xl'),
+          fontWeight: 'bold',
+          letterSpacing: '0.1rem',
+          lineHeight: '1',
+          marginBottom: '2.1rem'
         },
         h2: {
-          fontSize: theme('fontSize.3xl'),
+          fontFamily: theme('fontFamily.serif'),
+          fontSize: theme('fontSize.4xl'),
+          fontWeight: 'bold',
+          letterSpacing: '0.1rem',
+          lineHeight: '1.2',
           marginBottom: '2rem',
-          marginTop: '0.5rem',
-          fontWeight: 'bold'
+          marginTop: '0.5rem'
         },
         h3: {
-          fontSize: theme('fontSize.2xl'),
+          fontSize: theme('fontSize.3xl'),
+          fontFamily: theme('fontFamily.serif'),
+          letterSpacing: '0.07rem',
           marginBottom: '2rem',
           marginTop: '0.5rem',
           fontWeight: 'bold'
         },
         h4: {
           fontSize: theme('fontSize.xl'),
+          fontFamily: theme('fontFamily.serif'),
+          letterSpacing: '0.09rem',
           marginBottom: '2rem',
           marginTop: '0.5rem',
           fontWeight: 'bold'
         },
         h5: {
           fontSize: theme('fontSize.lg'),
+          fontFamily: theme('fontFamily.serif'),
+          letterSpacing: '0.09rem',
           marginBottom: '2rem',
           marginTop: '0.5rem',
           fontWeight: 'bold'
         },
         h6: {
           fontSize: theme('fontSize.lg'),
+          fontFamily: theme('fontFamily.serif'),
+          letterSpacing: '0.09rem',
           marginBottom: '2rem',
           marginTop: '0.5rem',
           fontWeight: 'bold'
@@ -80,7 +90,7 @@ module.exports = {
         p: {
           fontSize: theme('fontSize.xl'),
           marginBottom: '1.7rem',
-          lineHeight: '1.7'
+          lineHeight: '1.6'
         },
         a: {textDecoration: 'underline'},
         ul: {marginBottom: '1.5rem'},
