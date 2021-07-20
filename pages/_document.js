a44eac48e22e5ee6a6f88c65a0cf0165ebfc6119
Greg Rickaby <gregrickaby@gmail.com>
2021-07-20 14:46:06 -0500

    preload font stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -16,18 +16,26 @@ export default class MyDocument extends Document {
             name="google-site-verification"
             content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
           />
-          <link rel="preload" as="image" href="/blog/authors/greg.webp" />
           <link rel="preconnect" href="https://www.google-analytics.com" />
           <link rel="preconnect" href="https://www.googletagmanager.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Roboto&family=Suez+One&family=Material+Icons&display=swap"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&family=Suez+One&family=Material+Icons&display=swap"
+            rel="stylesheet"
+            crossOrigin="true"
+          />
+          <link rel="preload" as="image" href="/blog/authors/greg.webp" />
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
           <link rel="apple-touch-icon" href="/favicon/icon.png" />
           <link rel="icon" href="/favicon/icon.png" sizes="192x192" />
           <meta name="msapplication-TileImage" content="/favicon/icon.png" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto&family=Suez+One&family=Material+Icons&display=swap"
-            rel="stylesheet"
-          />
+
           <script
             defer
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
