d9e2aa9f56935602336bb185243eedb8b32836c9
Greg Rickaby <gregrickaby@gmail.com>
2021-08-05 21:15:02 -0500

    dark mode input adjustments

--- components/atoms/DarkMode/DarkMode.js
+++ components/atoms/DarkMode/DarkMode.js
@@ -20,28 +20,30 @@ export default function DarkMode() {
   })
 
   return (
-    <div className="flex fixed top-0 right-0 p-4">
-      <input
-        aria-label="Toggle theme color"
-        checked={darkMode.value}
-        className="sr-only"
-        id="checkbox"
-        name="checkbox"
-        onChange={darkMode.toggle}
-        type="checkbox"
-      />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="cursor-pointer"
-        enableBackground="new 0 0 24 24"
-        fill={true === darkMode.value ? '#d3d3d3' : '#1c1b1b'}
-        height="24px"
-        viewBox="0 0 24 24"
-        width="24px"
-      >
-        <rect fill="none" height="24" width="24" />
-        <path d="M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36c-0.98,1.37-2.58,2.26-4.4,2.26 c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z" />
-      </svg>
+    <div className="flex absolute top-0 right-0 p-4">
+      <label htmlFor="checkbox">
+        <input
+          aria-label="Toggle theme color"
+          checked={darkMode.value}
+          className="sr-only"
+          id="checkbox"
+          name="checkbox"
+          onChange={darkMode.toggle}
+          type="checkbox"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="cursor-pointer"
+          enableBackground="new 0 0 24 24"
+          fill={true === darkMode.value ? '#535353' : '#d3d3d3'}
+          height="36px"
+          viewBox="0 0 24 24"
+          width="36px"
+        >
+          <rect fill="none" height="24" width="24" />
+          <path d="M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36c-0.98,1.37-2.58,2.26-4.4,2.26 c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z" />
+        </svg>
+      </label>
     </div>
   )
 }
