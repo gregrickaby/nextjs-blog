b7699a24ad210f22daa1f067571967b4c5cc284b
Greg Rickaby <gregrickaby@gmail.com>
2022-05-25 12:11:39 -0500

    bring back the homepage

---
+++ pages/articles.js
@@ -0,0 +1,96 @@
+import Card from '@/components/molecules/Card/Card'
+import PageHeader from '@/components/molecules/PageHeader/PageHeader'
+import Layout from '@/components/templates/Layout/Layout'
+import config from '@/lib/config'
+import {POSTS_PATH} from '@/lib/helpers'
+import {getAllPosts} from '@/lib/posts'
+import {generateRssFeed} from '@/scripts/generate-rss'
+import PropTypes from 'prop-types'
+import {useState} from 'react'
+
+/**
+ * Render the BlogArchive component.
+ *
+ * @author Greg Rickaby
+ * @param {object} props       The component attributes as props.
+ * @param {any}    props.posts The post data.
+ * @return {Element} The BlogArchive component.
+ */
+export default function BlogArchive({posts}) {
+  const [query, setQuery] = useState('')
+
+  // Filter the posts by the query value.
+  const searchResults = posts.filter((post) =>
+    post?.data?.title?.toLowerCase().includes(query.toLowerCase())
+  )
+
+  return (
+    <Layout
+      title={`Articles - ${config?.siteName}`}
+      description="The latest articles on web development, photography, projects, and personal stuff."
+    >
+      <PageHeader title="Articles" />
+      <div className="grid gap-8">
+        <input
+          aria-label="Search all articles"
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search all articles"
+          className="rounded-sm bg-gray-100 py-3 px-4 dark:bg-gray-700 dark:text-white"
+        />
+
+        {
+          /* On initial page load, render all blog posts. */
+          !query && !!posts?.length && (
+            <>
+              {posts.map((post, index) => (
+                <Card key={index} {...post} path="article" />
+              ))}
+            </>
+          )
+        }
+
+        {
+          /* Render the search results. */
+          !!searchResults?.length && (
+            <>
+              <h3 className="mb-0">Search Results:</h3>
+              {searchResults.map((post, index) => (
+                <Card key={index} {...post} path="article" />
+              ))}
+            </>
+          )
+        }
+
+        {
+          /* No search results? Render a message. */
+          !searchResults.length && (
+            <p className="font-bold">Bummer. No articles found.</p>
+          )
+        }
+      </div>
+    </Layout>
+  )
+}
+
+BlogArchive.propTypes = {
+  posts: PropTypes.any.isRequired
+}
+
+/**
+ * Get static props.
+ *
+ * @author Greg Rickaby
+ * @see https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation
+ * @return {object} All post props.
+ */
+export async function getStaticProps() {
+  const posts = getAllPosts(POSTS_PATH)
+  await generateRssFeed()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,111 @@
-import Card from '@/components/molecules/Card/Card'
-import PageHeader from '@/components/molecules/PageHeader/PageHeader'
 import Layout from '@/components/templates/Layout/Layout'
 import config from '@/lib/config'
-import {POSTS_PATH} from '@/lib/helpers'
-import {getAllPosts} from '@/lib/posts'
-import {generateRssFeed} from '@/scripts/generate-rss'
-import PropTypes from 'prop-types'
-import {useState} from 'react'
+import Link from 'next/link'
 
 /**
- * Render the BlogArchive component.
+ * Render the Homepage page.
  *
  * @author Greg Rickaby
- * @param {object} props       The component attributes as props.
- * @param {any}    props.posts The post data.
- * @return {Element} The BlogArchive component.
+ * @return {Element} The Homepage component.
  */
-export default function BlogArchive({posts}) {
-  const [query, setQuery] = useState('')
-
-  // Filter the posts by the query value.
-  const searchResults = posts.filter((post) =>
-    post?.data?.title?.toLowerCase().includes(query.toLowerCase())
-  )
-
+export default function Homepage() {
   return (
     <Layout
-      title={`Articles - ${config?.siteName}`}
-      description="The latest posts on code, projects, and personal stuff."
+      title={`${config?.siteName} - Personal Blog`}
+      description={config?.ogDescription}
+      openGraph={{
+        title: `${config?.siteName} - Personal Blog`
+      }}
     >
-      <PageHeader title="Articles" />
-      <div className="grid gap-8">
-        <input
-          aria-label="Search all articles"
-          type="text"
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search all articles"
-          className="rounded-sm bg-gray-100 py-3 px-4 dark:bg-gray-700 dark:text-white"
-        />
+      <article>
+        <h2>About</h2>
+        <p>
+          Way back in 1997, I built my first website on Geocities and have been
+          passionate about the web since! In my spare time, I am an{' '}
+          <Link href="/books">
+            <a>published author</a>
+          </Link>
+          , <a href="https://www.gregrickaby.com">photographer</a>, and an owner
+          of a <a href="https://www.packers.com/">professional football team</a>
+          .
+        </p>
 
-        {
-          /* On initial page load, render all blog posts. */
-          !query && !!posts?.length && (
-            <>
-              {posts.map((post, index) => (
-                <Card key={index} {...post} path="article" />
-              ))}
-            </>
-          )
-        }
+        <h3>Photography</h3>
+        <p>
+          In 2018, I started to take a deep interest in photography. I bought a
+          mirrorless camera and spent months of taking online courses, reading
+          books, practicing, and having my work critiqued by peers. What has
+          started as an enjoyable hobby, has turned into a passionate way to
+          express my creativity.
+        </p>
 
-        {
-          /* Render the search results. */
-          !!searchResults?.length && (
-            <>
-              <h3 className="mb-0">Search Results:</h3>
-              {searchResults.map((post, index) => (
-                <Card key={index} {...post} path="article" />
-              ))}
-            </>
-          )
-        }
+        <h3>Web Development</h3>
+        <p>
+          I&apos;m a WordPress Technical Lead at{' '}
+          <a href="https://americaneagle.com/">AmericanEagle.com,</a> an
+          industry leader in web design, development, hosting, and digital
+          marketing with a passionate belief in the power of technology to
+          positively transform business practices. I lead a team of engineers as
+          we partner with clients to rebuild their frontends with WordPress and
+          Next.js.
+        </p>
 
-        {
-          /* No search results? Render a message. */
-          !searchResults.length && (
-            <p className="font-bold">Bummer. No articles found.</p>
-          )
-        }
-      </div>
+        <h3>Books</h3>
+        <p>
+          I contract with <em>For Dummies</em> and <em>WROX</em> brands to write
+          and edit technical books. I wrote a children&apos;s book in 2017
+          titled{' '}
+          <a href="https://amzn.to/34QAxAQ" rel="nofollow">
+            <em>Creating a Website for Dummies Jr</em>
+          </a>
+          . It guides the reader through the website planning and development
+          process. My book has been translated into other languages, and
+          currently has a near 5-star rating on Amazon.
+        </p>
+
+        <p>I also served as the Technical Editor for three other books:</p>
+        <ul>
+          <li>
+            <a href="https://amzn.to/37XCflU" rel="nofollow">
+              <em>WordPress for Dummies</em>
+            </a>{' '}
+            (Sabin-Wilson, 2021)
+          </li>
+          <li>
+            <a href="https://amzn.to/2BgCg7i" rel="nofollow">
+              <em>Professional WordPress Plugin Development</em>
+            </a>{' '}
+            (Williams et al., 2020)
+          </li>
+          <li>
+            <a href="https://amzn.to/37MMDLp" rel="nofollow">
+              <em>WordPress All-In-One For Dummies</em>
+            </a>{' '}
+            (Sabin-Wilson, 2019)
+          </li>
+        </ul>
+
+        <h3>Contributions</h3>
+        <p>
+          I&apos;ve been part of the WordPress community since 2008. I&apos;ve
+          made contributions to core, documentation, plugins, and themes.
+          I&apos;ve also spoken at WordCamps and meet-ups. I&apos;m also a
+          contributor to other open-source projects like Next.js, Storybook, and
+          Gatsby.
+        </p>
+        <p>
+          Outside of tech, I volunteer my time and efforts at local community
+          organizations. The Boy Scouts, Wiregrass Church, and the Enterprise
+          High School Band Boosters to name a few.
+        </p>
+        <p>
+          You can find me around the web with the links below. Stay safe out
+          there, and thanks for reading!
+          <span role="img" aria-label="cheers">
+            🍻
+          </span>
+        </p>
+      </article>
     </Layout>
   )
-}
-
-BlogArchive.propTypes = {
-  posts: PropTypes.any.isRequired
-}
-
-/**
- * Get static props.
- *
- * @author Greg Rickaby
- * @see https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation
- * @return {object} All post props.
- */
-export async function getStaticProps() {
-  const posts = getAllPosts(POSTS_PATH)
-  await generateRssFeed()
-
-  return {
-    props: {
-      posts
-    }
-  }
 }
