b53b5c0ada62f8ef1e7b24e6485550a79fa95e5e
Greg Rickaby <gregrickaby@gmail.com>
2022-05-25 11:52:03 -0500

    update bio and avatar

--- components/molecules/AuthorCard/AuthorCard.js
+++ components/molecules/AuthorCard/AuthorCard.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import siteLogo from '../../../public/blog/authors/greg-original.jpg'
+import siteLogo from '../../../public/blog/authors/greg-rickaby.jpg'
 import styles from './AuthorCard.module.css'
 
 export default function AuthorCard() {
@@ -17,12 +17,8 @@ export default function AuthorCard() {
         />
       </div>
       <div className={styles.info}>
-        Greg is the Director of Engineering at{' '}
-        <a href="https://webdevstudios.com">WebDevStudios</a>. He also
-        moonlights at <a href="https://www.dummies.com/">Dummies</a> writing and
-        editing books.{' '}
-        <a href="https://twitter.com/gregrickaby">Follow him on Twitter</a> for
-        lots of pictures of pepperoni pizza and tidbits about Next.js.
+        Greg is an author, photographer, dad, husband, and WordPress Technical
+        Lead at <a href="https://americaneagle.com">AmericanEagle.com</a>.
       </div>
     </div>
   )
