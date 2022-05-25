bf78d215eccbfa7c2f93e57811705f9989c0c7f2
Greg Rickaby <gregrickaby@gmail.com>
2022-05-25 12:11:21 -0500

    update title, avatar, socials, etc...

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,29 @@ export default class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/favicon/icon.png" />
           <link rel="icon" href="/favicon/icon.png" sizes="192x192" />
           <meta name="msapplication-TileImage" content="/favicon/icon.png" />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: `{
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Greg Rickaby",
+              "url": "https://gregrickaby.blog",
+              "sameAs": [
+                "https://500px.com/p/gregrickaby",
+                "https://codepen.io/gregrickaby",
+                "https://github.com/gregrickaby",
+                "https://linkedin.com/in/gregrickaby",
+                "https://profiles.wordpress.org/gregrickaby"
+                "https://twitter.com/gregrickaby",
+                "https://www.amazon.com/author/gregrickaby",
+                "https://www.goodreads.com/gregrickaby",
+                "https://www.gregrickaby.com",
+                "https://www.youtube.com/c/GregRickaby",
+              ]
+            }`
+            }}
+          />
         </Head>
         <body>
           <Main />
